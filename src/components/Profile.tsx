24bd9e9eb6b1c2c7dda91c9b3f2541ebdc682375
felipe.dominicheli <felipe.dominicheli@tegra.com.br>
2023-05-20 15:46:36 -0300

    creation of routes to create a new memory and send an image or video

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import { getUser } from '@/lib/auth'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function Profile() {
   const { name, avatarUrl } = getUser()
@@ -16,7 +17,10 @@ export function Profile() {
 
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-purple-400 hover:text-purple-300">
+        <a
+          href="/api/auth/logout"
+          className="block text-purple-400 hover:text-purple-300"
+        >
           Quero sair
         </a>
       </p>
